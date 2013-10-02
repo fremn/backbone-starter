66986645e32a496c4677e80f3809fa17f564fb7b
leejaew <lee.jaewang@gmail.com>
2013-10-02 15:31:14 -0500

    create underscore templates to render on html elements

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+// 1
 // var DrinkView = Backbone.View.extend({
 //   initialize: function(options){
 //   	this.name = options.name
@@ -16,6 +18,7 @@
 //   name: 'Water'
 // });
 // water.render();
+
 // 2
 // var ButtonView = Backbone.View.extend({
 //   render: function () {
@@ -29,48 +32,92 @@
 // view.render();
 
 // 3
+// var ButtonView = Backbone.View.extend({
+//   render: function () {
+//     $(this.el).html('new shtuff');
+//   }
+// });
+
+// var view = new ButtonView({
+//   el: $('.big-button')
+// });
+// view.render();
+
+// // 4
+// var stockTemplate = _.template('<b><%= name %>:</b> <%= price %>');
+
+// var StockView = Backbone.View.extend({
+//   initialize: function (options) {
+//     this.name = options.name;
+//     this.price = options.price;
+//     this.options = options
+//   },
+
+//   render: function () {
+//     $(this.el).html(stockTemplate(this.options));
+//   }
+// });
+
+// var aapl = new StockView({
+//   name: 'AAPL',
+//   price: 380,
+//   el: $('.stock-price:eq(0)')
+// });
+// var goog = new StockView({
+//   name: 'GOOG',
+//   price: 800,
+//   el: $('.stock-price:eq(1)')
+// });
+// aapl.render();
+// goog.render();
+
+//5
+// var dressTemplateHtml = $('#templates .dress').html();
+// var dressTemplate = _.template(dressTemplateHtml);
+
+// var DressView = Backbone.View.extend({
+//   className: 'dress',
+
+//   initialize: function (options) {
+//     this.color = options.color;
+//     this.price = options.price;
+//   },
+
+//   render: function () {
+//     var newDressHtml = dressTemplate({ color: this.color, price: this.price });
+//     $(this.el).html(newDressHtml);
+//   }
+// });
+
+// var dressOne = new DressView({
+//   color: 'green',
+//   price: 14.99
+// });
+// var dressTwo = new DressView({
+//   color: 'red',
+//   price: 18.99
+// });
+
+// dressOne.render();
+// dressTwo.render();
+// $('body').append(dressOne.el);
+// $('body').append(dressTwo.el);
+
+//6
+var buttonTemplateHtml = $('#templates .button').html();
+var buttonTemplate = _.template(buttonTemplateHtml);
+
 
 var ButtonView = Backbone.View.extend({
+
   render: function () {
-    $(this.el).html('new shtuff');
+    var newButtonHtml = buttonTemplate({ buttonText: 'backbone iz coo' });
+    $(this.el).html(newButtonHtml);
   }
 });
 
-var view = new ButtonView({
-  el: $('.big-button')
+btn = new ButtonView({
+	el: $('.display')
 });
-view.render();
-
-// 4
-
-var stockTemplate = _.template('<b><%= name %>:</b> <%= price %>');
-
-var StockView = Backbone.View.extend({
-  initialize: function (options) {
-    this.name = options.name;
-    this.price = options.price;
-    this.options = options
-  },
-
-  render: function () {
-    $(this.el).html(stockTemplate(this.options));
-  }
-});
-
-var aapl = new StockView({
-  name: 'AAPL',
-  price: 380,
-  el: $('.stock-price:eq(0)')
-});
-var goog = new StockView({
-  name: 'GOOG',
-  price: 800,
-  el: $('.stock-price:eq(1)')
-});
-aapl.render();
-goog.render();
-
-
-
-
+btn.render();
 
